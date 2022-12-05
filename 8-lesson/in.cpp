e39424facb3c72b83231bc47a74339c4c4268373
Alexandr Isaenkov <isaenkovcc@mail.ru>
2022-12-05 16:00:47 +0300

    fixing 8th

--- 8-lesson/in.cpp
+++ 8-lesson/in.cpp
@@ -8,5 +8,18 @@ using namespace std;
 
 int main()
 {
+    ifstream of("MyTxt.txt");
+    selfVector<int> rf;
+    int size = 0;
+    of.rdbuf()->sgetn(reinterpret_cast<char*>(&size), sizeof(size));
+    rf.resize(size);
+    for (int i = 0; i < size; ++i)
+    {
+        of.rdbuf()->sgetn(reinterpret_cast<char*>(&rf[i]), sizeof(int));
+    }
 
+    for (int i = 0; i < size; ++i)
+    {
+        cout << rf[i] << " ";
+    }
 }
