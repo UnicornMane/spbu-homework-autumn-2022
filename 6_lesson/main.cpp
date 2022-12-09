a6d849b35e3a2b0bc29e7bf0a0563ae1cde4ff12
Alexandr Isaenkov <isaenkovcc@mail.ru>
2022-12-09 20:06:55 +0300

    uploading new homework part 1

--- 6_lesson/main.cpp
+++ 6_lesson/main.cpp
@@ -1,27 +1,46 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <vector>
+#include "selfVector1.h"
 
 using namespace std;
 
+
 int main(int argc, char  *argv[])
 {
-    try
-    {
-        std::fstream fs;
-        fs.open("//Users//mi//Documents/", std::fstream::in | std::fstream::out | std::fstream::app);
-        if (!fs.is_open())
-        {
-            throw std::fstream::failure("Unable to run .txt");
-        }
+//    ofstream fout;
+//    fout.exceptions(ofstream::badbit | ofstream::failbit);
+//
+//    string fileName = "";
+//    cin >> fileName;
+//    try
+//    {
+//        fout.open(fileName);
+//    }
+//    catch (const ofstream::failure &ex)
+//    {
+//        cout << "bad file name\n";
+//    }
 
-    }
-    catch(std::exception &ex)
-    {
-        if (typeid(ex) == typeid(std::ifstream::failure))
-        {
-            std::cout << ex.what();
-        }
-    }
+
+    string tmp = "";
+    cin >> tmp;
+    int n = 0;
+    n = stoi(tmp);
+//    try
+//    {
+//        n = stoi(tmp);
+//    }
+//    catch (const invalid_argument &ex)
+//    {
+//        cout << "not an int type\n";
+//    }
+    selfVector<int> f(n);
+
+    cout << f[0];
+
+
+
     return EXIT_SUCCESS;
 }
